333ca6e30185b3afb5fa034b744dcd657170af68
RollerMotion <info@rollermotion.com.ar>
2026-02-19 14:50:58 -0300

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,10 @@
+export function roundUp(value) {
+    return Math.ceil(value / 100) * 100;
+}
+
+export function formatCurrency(value) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(value);
+}
